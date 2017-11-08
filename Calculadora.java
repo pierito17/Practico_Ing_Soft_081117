a68505a08b1c0dd3b6d9d51f4d31fb46a24be56d
alumnoFI <alumnoFI@facultades.ort.edu.uy>
2017-11-08 08:24:05 -0300

    Soporte para resta

--- Calculadora.java
+++ Calculadora.java
@@ -5,8 +5,14 @@ if(args.length==3){
 try{
 int op1 = Integer.parseInt(args[1]);
 int op2 = Integer.parseInt(args[2]);
-
+if("+".equals(args[0])){
 System.out.println(op1 + op2);
+}else if("-".equals(args[0])){
+System.out.println(op1 - op2);
+}else{
+	System.out.println(args[0] + ": operacion desconocida.");
+}
+
 } catch(Exception ex){
 System.out.println("Parametros incorrectos");
 }
